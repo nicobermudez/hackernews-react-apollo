e430f5c9645cd7cb2d83524fa2a9d3adba7175e0
Nico Bermudez <bermnicolas@gmail.com>
2019-07-19 11:56:13 -0400

    Add Link and LinkList components and connect client and server

--- src/index.js
+++ src/index.js
@@ -6,21 +6,21 @@ import * as serviceWorker from './serviceWorker';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
-import { inMemoryCache } from 'apollo-cache-inmemory';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 })
 
 const client = new ApolloClient({
-  link: httpLink;
-  cache: new inMemoryCache()
+  link: httpLink,
+  cache: new InMemoryCache()
 })
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>, 
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
